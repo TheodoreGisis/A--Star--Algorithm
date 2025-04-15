c57109e16a4d288e142f6c8ddbdb0135f5991b62
Gkisis <Theodoros.Gkisis@chubb.com>
2025-04-15 16:02:21 +0300

    Change folder name

---
+++ app.py
@@ -0,0 +1,47 @@
+import subprocess
+
+def list_git_folders():
+    try:
+        result = subprocess.run(['git', 'ls-tree', '-r', '--name-only', 'HEAD'], capture_output=True, text=True, check=True)       
+        
+        files = result.stdout.splitlines()
+        folders = set()
+
+        for file in files:
+            folder = '/'.join(file.split('/')[:-1])  
+            if folder:  
+                folders.add(folder)
+
+        if folders:
+            print("Git folders:")
+            for folder in sorted(folders):
+                print(f"- {folder}")
+        else:
+            print("No folders found in the Git repository.")
+
+        return folders
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error running git ls-tree: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def rename_folder():
+    old_folder_name = "Visualize"
+    new_folder_name = "test"
+
+    try:
+        naming_command = subprocess.run(['git' , 'mv' , old_folder_name , new_folder_name])
+        commit_message("Change folder name")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error running git mv command: {e}")
+
+
+def commit_message(message):
+    commiting_result = subprocess.run(['git' , 'commit' , '-m', message])
+
+
+if __name__ == "__main__":
+    rename_folder()
